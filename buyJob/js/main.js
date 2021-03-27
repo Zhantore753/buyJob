d572f9a65961cda1ee893dbd3a3e5e1a22bcdbd2
Zhantore <753gg@mail.ru>
2021-03-27 09:59:04 +0600

    repeatable footer

--- buyJob/js/main.js
+++ buyJob/js/main.js
@@ -2,8 +2,6 @@ const burger = document.querySelector('.nav__burger');
 const header = document.querySelector('.header');
 
 burger.addEventListener('click', ()=>{
-    window.scrollTo(pageXOffset, 0);
-
     document.body.classList.toggle('overflow-h');
     burger.classList.toggle('nav__burger-active');
     header.classList.toggle('header-active');
